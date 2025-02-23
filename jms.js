593d8b9ec0fe3b7ea314be47e27728d7da26c4bc
Blue Archive <119113753+Alice-Candy@users.noreply.github.com>
2025-02-23 16:57:29 +0800

    Update jms.js

--- jms.js
+++ jms.js
@@ -41,7 +41,7 @@ $httpClient.get(url, function(error, response, data) {
 
   const panel = {
     title: "✈️ 𝙅𝙈𝙎 𝙄𝙣𝙛𝙤",
-    content: `已使用流量：${usedGB}GB\n流量剩余：${remainingGB}GB\n重置日期：${nextResetMonth}月${bwResetDay}号`,
+    content: `已用流量：${usedGB}GB\n剩余流量：${remainingGB}GB\n重置日期：${nextResetMonth}月${bwResetDay}号`,
     icon: 'airplane.circle.fill',
     'icon-color': '#000000',
   };
